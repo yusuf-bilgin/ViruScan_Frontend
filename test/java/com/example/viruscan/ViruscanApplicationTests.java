b44ebcd8a4c0d8d584b5015641f5a0e4a3d79862
Yusuf <yusufbilgin1905@gmail.com>
2023-10-21 11:24:20 +0300

    Create ViruscanApplicationTests.java

---
+++ test/java/com/example/viruscan/ViruscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viruscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViruscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
